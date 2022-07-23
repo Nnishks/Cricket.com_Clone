2b602f5c1b43570bebde79918110f21c6c316d1a
aamirkhan9420 <aamirkhan.z9373@gmail.com>
2022-07-23 20:52:57 +0530

    add

--- fw18_1184/script/live.js
+++ fw18_1184/script/live.js
@@ -2,7 +2,7 @@ import { fet, appe } from "../components/livefetapp.js";
 let url = `https://api.cricapi.com/v1/currentMatches?apikey=361679f1-8c93-4bf4-aa57-d33b67950cac&offset=0`;
 let container = document.querySelector("#grid_box_body");
 const heading = document.querySelector("#gid_box_head");
-let a;
+let a = 10;
 fet(url).then((res) => {
   console.log(res.data);
   appe(res.data, container, heading);
